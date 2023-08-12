bcb1cb1e8d027edc2ff6c95cde03e9d467f8935b
hollowheights <36546351+hollowheights@users.noreply.github.com>
2023-08-12 23:51:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
-df_airports = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv")
+df_airports = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv"
+)
 
 app = Dash(__name__)
 
 server = app.server
 
 
+
 app.layout = html.Div(
     [
-        html.title("Test Page")
         html.H4("Airports"),
         html.P(
             "px.scatter_geo is used to plot points on globe across geolocations while "
+            "px.scatter_mapbox is used to plot points on map across geolocations."
         ),
         dcc.Graph(id="graph"),
         html.P(
@@ -35,22 +38,32 @@ app.layout = html.Div(
     Input("type", "value"),
 )
 def generate_chart(values):
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    if values == "scatter_geo":
+        # changing so that the default position will be US
+        fig = px.scatter_geo(
+            df_airports,
+            locationmode="USA-states",
+            lat="lat",
+            lon="long",
+            hover_data=["airport", "city", "state", "cnt"],
+            color="cnt",
+            color_continuous_scale=px.colors.cyclical.IceFire,
+            projection="orthographic",
+        )
 
-    fig = px.scatter_mapbox(
-        df_airports,
-        lat="lat",
-        lon="long",
-        hover_data=["airport", "city", "state", "cnt"],
-        size="cnt",
-        color="cnt",
-        zoom=3,
-    )
-    fig.update_layout(mapbox_style="open-street-map")
-
-    # Set the size of the plot
-    fig.update_layout(width=1000, height=1000)
-
+        fig.update_layout(mapbox_style="open-street-map")
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    else:
+        fig = px.scatter_mapbox(
+            df_airports,
+            lat="lat",
+            lon="long",
+            hover_data=["airport", "city", "state", "cnt"],
+            size="cnt",
+            color="cnt",
+            zoom=3,
+        )
+        fig.update_layout(mapbox_style="open-street-map")
     return fig
 
 
