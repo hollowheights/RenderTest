a4c2dfabd936084fe421fa9a5ab7e4b12e131887
hollowheights <36546351+hollowheights@users.noreply.github.com>
2023-08-12 23:29:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def generate_chart(values):
         fig.update_layout(mapbox_style="open-street-map")
         
         # Set the size of the plot
-        fig.update_layout(width=800, height=400)
+        fig.update_layout(width=800, height=1000)
 
         fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     else:
