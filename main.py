08147f507351e562e25825ec6f87cbc5beb29c4e
hollowheights <36546351+hollowheights@users.noreply.github.com>
2023-08-13 15:34:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 
@@ -7,14 +7,45 @@ df_airports = pd.read_csv(
 )
 
 app = Dash(__name__)
-app.title = "SR Test App"
 
-server = app.server
+fig = px.scatter_mapbox(
+    df_airports,
+    lat="lat",
+    lon="long",
+    hover_data=["airport", "city", "state", "cnt"],
+    size="cnt",
+    color="cnt",
+    zoom=3,
+)
+fig.update_layout(mapbox_style="open-street-map")
 
 app.layout = html.Div(
     [
-        dcc.Graph(id="graph"),
+        html.H4("Airports"),
+        dcc.Graph(id="graph", figure=fig),
     ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+
+'''
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
+
+df_airports = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv")
+
+app = Dash(__name__)
+app.title = "SR Test App"
+server = app.server
+
+app.layout = html.Div(
+    [dcc.Graph(id="graph")],
+    
+    
+
     fig = px.scatter_mapbox(
         df_airports,
         lat="lat",
@@ -28,7 +59,7 @@ app.layout = html.Div(
     # Set the size of the plot
     fig.update_layout(width=1500, height=900)
 )
-    
+'''    
 
 if __name__ == "__main__":
     app.run_server(debug=True)
